98661d0af88f06a1503ba86f71b9c2571651c84f
Yasin Ayeb <yasin.ayeb@yh.nackademin.se>
2024-01-11 11:58:56 +0100

    Started product list item component

---
+++ src/components/ProductListItem.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+const Box = styled.div`
+   widht:50px;
+   height: 50px;
+   background-color: dodgerblue;
+`
+const Btn = styled.button`
+
+`
+
+function ProductListItem() {
+    return (
+        <Box>
+           product list item
+        </Box>
+    )
+}
+
+export default ProductListItem
